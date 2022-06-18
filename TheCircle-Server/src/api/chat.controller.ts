95f77fa8e746118191372afea10e716d0d918bab
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-06-18 15:42:33 +0200

    Back-end: re-add chats

---
+++ TheCircle-Server/src/api/chat.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Get, NotFoundException, Param, ParseUUIDPipe, Post, ValidationPipe } from "@nestjs/common";
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { validationOptions } from "src/app.constants";
+import { MessageEntity } from "src/data/message.entity";
+import { MessageService } from "src/data/message.service";
+import { MessageAddDto } from "./message-add.dto";
+import { MessageReadDto } from "./message-read.dto";
+
+@ApiTags("chat")
+@Controller({
+    path: "chat",
+    version: "1"
+})
+export class ChatController {
+    constructor (
+        private readonly messageService: MessageService
+    ) {}
+
+    @Get(":chatId/messages")
+    @ApiOkResponse({ type: [MessageReadDto] })
+    @ApiBadRequestResponse()
+    async getMessagesForChat(
+        @Param("chatId", new ParseUUIDPipe()) chatId: string
+    ): Promise<MessageReadDto[]> {
+        const messages = await this.messageService.getMessagesForChat(chatId)
+        return messages
+    }
+
+    @Get(":chatId/messages/:messageId")
+    @ApiNotFoundResponse()
+    @ApiOkResponse({ type: MessageReadDto })
+    @ApiBadRequestResponse()
+    async getMessageById(
+        @Param("chatId", new ParseUUIDPipe()) chatId: string,
+        @Param("messageId", new ParseUUIDPipe()) messageId: string
+    ): Promise<MessageReadDto> {
+        const message = await this.messageService.findByMessageId(messageId)
+
+        if (message == null || message.chatId != chatId) {
+            throw new NotFoundException()
+        }
+
+        return message
+    }
+
+    @Post()
+    @ApiCreatedResponse({ type: MessageReadDto })
+    @ApiBadRequestResponse()
+    async createMessage(
+        @Body(new ValidationPipe(validationOptions)) messageAddDto: MessageAddDto
+    ): Promise<MessageReadDto> {
+        const messageEntity = new MessageEntity()
+        messageEntity.chatId = messageAddDto.chatId
+        messageEntity.senderId = messageAddDto.senderId
+        messageEntity.message = messageAddDto.message
+        messageEntity.senderSignature = messageAddDto.senderSignature
+        return await this.messageService.create(messageEntity)
+    }
+}
---
+++ TheCircle-Server/src/api/message-read.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsDateString, IsString, IsUUID } from "class-validator"
+
+export class MessageReadDto {
+    @ApiProperty()
+    @IsUUID()
+    messageId: string
+
+    @ApiProperty()
+    @IsUUID()
+    chatId: string
+
+    @ApiProperty()
+    @IsUUID()
+    senderId: string
+
+    @ApiProperty()
+    @IsString()
+    message: string
+
+    @ApiProperty()
+    @IsDateString()
+    creationDate: string
+
+    @ApiProperty()
+    @IsString()
+    senderSignature: string
+}
